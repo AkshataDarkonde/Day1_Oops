d4cac556d1a9f58fdd8d257e58ab6675d6c19c74
AkshataDarkonde <91557235+AkshataDarkonde@users.noreply.github.com>
2023-11-20 21:03:55 +0530

    Add files via upload

---
+++ DataBase/src/Programs/ArraySearch.java
@@ -0,0 +1,24 @@
+package Programs;
+
+public class ArraySearch {
+    public static void main(String[] args) {
+        int[] numbers = {5, 12, 14, 6, 78, 19, 23, 26, 35, 37, 7, 52, 86, 47}; // Array of size 15
+        int searchNumber = 19; // Number to be searched
+
+        boolean found = false;
+
+        // Search for the number in the array
+        for (int num : numbers) {
+            if (num == searchNumber) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Number " + searchNumber + " is present in the array.");
+        } else {
+            System.out.println("Number " + searchNumber + " is not present in the array.");
+        }
+    }
+}
